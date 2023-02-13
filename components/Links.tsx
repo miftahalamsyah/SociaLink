5a2298fd351f889bb60d504dcc609088e1a28189
Miftah Rizky Alamsyah <50770423+miftahalamsyah@users.noreply.github.com>
2023-02-14 00:27:48 +0700

    Update Links.tsx

--- components/Links.tsx
+++ components/Links.tsx
@@ -11,6 +11,8 @@ export const Links: NextPage = () => {
                 <button className="raise" onClick={() => window.open("https://instagram.com/miftah.rzky", "_blank")}>Instagram</button>
                 <button className="raise" onClick={() => window.open("https://www.linkedin.com/in/miftah-rzky/", "_blank")}>LinkedIn</button>
                 <button className="raise" onClick={() => window.open("https://miftahalamsyah.my.id", "_blank")}>Portfolio</button>
+                <button className="raise" onClick={() => window.opem("https://www.youtube.com/@miftahrzky", "_blank")}>YouTube</button>
+
             </div>
         </>
     );
